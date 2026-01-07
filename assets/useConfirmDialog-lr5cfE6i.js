2c6276658c65dfc57fb4a6c9807f122f00d44dad
webdev0320 <muhammadarbaz894@gmail.com>
2026-01-07 13:02:44 +0500

    Updates

--- assets/useConfirmDialog-lr5cfE6i.js
+++
@@ -1,1 +0,0 @@
-import{r as s,cn as o}from"./index-DIY64fxg.js";const r=s(!1),e=o({title:"Confirm",message:"Are you sure?",resolve:null});function a(){r.value=!1,e.resolve&&e.resolve(!0)}export{a as c};
