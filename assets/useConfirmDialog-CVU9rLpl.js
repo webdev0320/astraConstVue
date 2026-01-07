d7d60d96d6f3411fbb9889bc1826e3352dc1d4d1
webdev0320 <muhammadarbaz894@gmail.com>
2026-01-07 17:36:39 +0500

    git push

---
+++ assets/useConfirmDialog-CVU9rLpl.js
@@ -0,0 +1,1 @@
+import{r as s,cn as o}from"./index-olpn7k_8.js";const r=s(!1),e=o({title:"Confirm",message:"Are you sure?",resolve:null});function a(){r.value=!1,e.resolve&&e.resolve(!0)}export{a as c};
